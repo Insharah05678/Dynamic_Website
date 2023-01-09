69e41b640fc5700f8a73aac840ec59b1d79311e3
Insharah Riaz <88031732+Insharah05678@users.noreply.github.com>
2023-01-09 16:47:17 +0500

    php

---
+++ ChoiceEvaluatorPage/index.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>JUW | FYP | Evaluator Selector</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!--link rel="stylesheet" href="./style.css"-->
+  </head>
+  <body>
+      <div class="hero-image">
+        <div class="hero-text">
+          <div class="fluid-container h-100 body-frame" >
+          <div class="row d-flex justify-content-center align-items-center" >
+            <div class="col-lg-9 col-xl-12">
+              <div class="card text-black">
+                <div class="text-black logoHeader">
+                  <img src="../images/JUW_Logo.png" alt="JUW" style="cursor: pointer; width: 12%;">
+                  <h>Jinnnah University For Women<pre style="font-size: 18px; ">          FYP Evaluation System</pre></h>
+                </div>
+                <div class="card-body p-md-5">
+                  <div class="row justify-content-center">
+                    <div class="col-md-10 col-lg-12 col-xl-19 order-2 order-lg-1">
+                      <p class="text-center h2 fw-bold mb-5 mx-1 mx-md-4 mt-9">Select For Role</p>
+                      <div class="row">
+                        <div class="col-sm-4">
+                          <div class="card">
+                            <div class="card-body">
+                              <img src="../images/v2.png" alt="Female-icon" style="border-radius: 50%; width:140px;">
+                              <h1></h1>
+                              <a href="#" class="btn btn-primary" onclick="location.href = '../Login Form/login.php';">Supervisor</a>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-sm-4">
+                          <div class="card">
+                            <div class="card-body">
+                              <img src="../images/v2.png" alt="Female-icon" style="border-radius: 50%; width:140px;">
+                              <h1></h1>
+                              <a href="#" class="btn btn-primary" onclick="location.href = '../Login Form/login.php';">External</a>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                              <div class="card-body">
+                                <img src="../images/v2.png" alt="Female-icon" style="border-radius: 50%; width:140px;">
+                                <h1></h1>
+                                <a href="#" class="btn btn-primary" onclick="location.href = '../Login Form/login.php';">Committee</a>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+     
+    </script>
+    
+  </body>
+</html>
+<!--
+  
+        
+-->
+
+<style>
+  /* Style*/
+  .hero-image {
+  background-image: url("../images/juwww.jpeg");
+  background-color: #c7c4c4;
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  opacity: 0.8;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: rgb(245, 232, 207);
+}
+
+.logoHeader{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: 30px;
+  text-align: left;
+  margin-top: 5px;
+   box-shadow: 5px 10px 18px #d5d5d5;
+   border-radius: 2%;
+}
+.btn{
+  margin-left: 20px;
+  width: 110px;
+}
+
+</style>
